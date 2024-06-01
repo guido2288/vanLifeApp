5ca57e1c1de2cfb202a42343a12c7ef5cb2a373b
adabati22 <guido2288@gmail.com>
2024-06-01 16:54:05 -0300

    HostVansPage added

--- pages/Host/HostVans.jsx
+++ pages/Host/HostVans.jsx
@@ -1,6 +1,31 @@
 const HostVans = () => {
   return (
-    <div>HostVans</div>
+    <div className="hostVans-container">
+      <h2>Your listed vans</h2>
+      <div className="hostVans-cardContainer">
+
+
+        <div className="hostVans-card">
+            <img src="https://assets.scrimba.com/advanced-react/react-router/modest-explorer.png" alt="" />
+            <div>
+              <h3>Modest Explorer</h3>
+              <span>$60/day</span>
+            </div>
+        </div>
+
+        <div className="hostVans-card">
+            <img src="https://assets.scrimba.com/advanced-react/react-router/dreamfinder.png" alt="" />
+            <div>
+              <h3>Modest Explorer</h3>
+              <span>$60/day</span>
+            </div>
+        </div>
+        
+
+      </div>
+
+
+    </div>
   )
 }
 
