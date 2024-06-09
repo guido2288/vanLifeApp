29df8188bb0f28012626cd534b322428ccb6fdf2
adabati22 <guido2288@gmail.com>
2024-06-09 15:39:44 -0300

    Protected Routes added

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,8 @@ import HostVanPhotos from "../pages/Host/HostVanPhotos"
 import HostVanPricing from "../pages/Host/HostVanPricing"
 import NotFound from "../components/NotFound"
 import Login from "../pages/Login"
+import Auth from "../pages/Login"
+import AuthRequired from "../components/AuthRequired"
 
 function App() {
 
@@ -34,18 +36,21 @@ function App() {
 
             <Route path="login" element={ <Login /> } />
 
-            <Route path="host" element={ <HostLayout /> }>
-            | <Route index element={ <Dashboard /> }/>
-  |           <Route path="income" element={ <Income /> }/>
-  |           <Route path="vans" element={ <HostVans /> }/>
-              <Route path="reviews" element={ <Reviews /> }/>
-  |           <Route path="vans/:id" element={ <HostVansDetails /> }>
-                <Route index  element={ <HostVanInfo /> }/>
-                <Route path="pricing" element={ <HostVanPricing /> }/>
-                <Route path="photos" element={ <HostVanPhotos /> }/>
+            <Route element={<AuthRequired /> } >
+              <Route path="host" element={ <HostLayout /> }>
+              | <Route index element={ <Dashboard /> }/>
+    |           <Route path="income" element={ <Income /> }/>
+    |           <Route path="vans" element={ <HostVans /> }/>
+                <Route path="reviews" element={ <Reviews /> }/>
+    |           <Route path="vans/:id" element={ <HostVansDetails /> }>
+                  <Route index  element={ <HostVanInfo /> }/>
+                  <Route path="pricing" element={ <HostVanPricing /> }/>
+                  <Route path="photos" element={ <HostVanPhotos /> }/>
+                </Route>
               </Route>
-              
             </Route>
+
+
             <Route path="*" element={ <NotFound/> }/>
           </Route>
         </Routes>
