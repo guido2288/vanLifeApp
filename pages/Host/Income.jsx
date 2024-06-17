148f4744584cb728edeabf7d2873d5fd5ab5c9bb
guido2288 <guido2288@gmail.com>
2024-06-17 15:46:00 -0300

    Reviews style added

--- pages/Host/Income.jsx
+++ pages/Host/Income.jsx
@@ -1,6 +1,35 @@
+import { Link } from "react-router-dom"
+
 const Income = () => {
+
+  const transactionsData = [
+    { amount: 720, date: "Jan 3, '23", id: "1" },
+    { amount: 560, date: "Dec 12, '22", id: "2" },
+    { amount: 980, date: "Dec 3, '22", id: "3" },
+  ]
+
   return (
-    <div>Income</div>
+    <>
+      <section className="host-income">
+          <div className="info">
+            <h1>Income</h1>
+              <p>Last <span>30 days</span></p>
+              <h2>$2,260</h2>
+              <h3>Your transactions (3)</h3>
+              <p>Last <span>30 days</span></p>
+          </div>
+
+
+          <div className="transactions">
+                {transactionsData.map((item) => (
+                    <div key={item.id} className="transaction">
+                        <h4>${item.amount}</h4>
+                        <p>{item.date}</p>
+                    </div>
+                ))}
+            </div>
+      </section>
+    </>
   )
 }
 
